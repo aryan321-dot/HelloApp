5a103f05c93816e0de8d67b8527ceaf073b5c9a9
aryan321-dot <getmearyan321@gmail.com>
2026-03-18 09:15:10 +0530

    UC2-changes

--- src/UC2.java
+++ src/UC2.java
@@ -1,11 +1,6 @@
 public class UC2 {
     public static void main(String[] args){
-        if (args.length > 0) {
-            String name = args[0];
-            System.out.println("Hello, " + name + " !");
-        } 
-        else {
-            System.out.println("Error: Please provide a name as a command-line argument.");
-        }
+        String name = args[0];
+        System.out.println("Hello, " + name + " !");
     }
 }
