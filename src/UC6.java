37e86e7867213000d820cce7f8ecbee81b753ef0
aryan321-dot <getmearyan321@gmail.com>
2026-03-18 10:19:48 +0530

    UC6- Changed

--- src/UC6.java
+++ src/UC6.java
@@ -1,27 +1,17 @@
-public class UC6 {
-    public static void main(String [] args){
+class UC6 {
+    void main(String [] args){
         StringBuilder nameBuilder = new StringBuilder();
-        String greeting;
-
         if (args.length == 0)
-            // Case 1: No arguments provided
-            greeting = "Hello, World!";
+            System.out.println("Hello, World!");
         else {
-            // Case 2: Use enhanced for loop to add EVERY name + delimiter
             for (String name : args)
                 nameBuilder.append(name).append(", ");
-
-
-            // Remove the trailing ", " (last 2 characters)
-            if (nameBuilder.length() > 0) {
-                // substring(start, end) where end is total length minus the comma and space
+            // Using .isEmpty() instead of .length() > 0
+            if (!nameBuilder.isEmpty()) {
+                // Trim the trailing ", "
                 String finalNames = nameBuilder.substring(0, nameBuilder.length() - 2);
-                greeting = "Hello, " + finalNames + "!";
+                System.out.println("Hello, " + finalNames + "!");
             }
-            else
-                greeting = "Hello, World!";
         }
-
-        System.out.println(greeting);
     }
 }
